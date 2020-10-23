93f232d847a221c63fb08e8f718075894bb1f6ff
João Pedro Schmitz <oi@joaopedro.cc>
2020-10-23 10:49:48 -0300

    initial commit

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const App = () => <h1>parcel-react-ts</h1>;
+const App = () => <h1>parcel-react-ts (PR from CodeSandbox)</h1>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
